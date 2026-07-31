c8440f2ed1189a82bdb701cbd8f3b0bb617a4711
leap4one-lgtm <leap4one@gmail.com>
2026-07-31 07:34:00 +0530

    Update test_transform.py

--- test_transform.py
+++ test_transform.py
@@ -1,5 +1,13 @@
-from transform import clean_numbers
+from transform import clean_numbers, average, total
 
 def test_clean_numbers():
     result = clean_numbers([3, 1, 2, 3, 1])
     assert result == [1, 2, 3]
+
+def test_average():
+    result = average([2, 4, 6])
+    assert result == 4
+
+def test_total():
+    result = total([1, 2, 3])
+    assert result == 6
