2729eda29426221706d4f050ce93444cdade9c04
leap4one-lgtm <leap4one@gmail.com>
2026-07-31 07:03:48 +0530

    Update transform.py

--- transform.py
+++ transform.py
@@ -1,3 +1,8 @@
 def clean_numbers(numbers):
     """Remove duplicates and sort a list of numbers."""
     return sorted(set(numbers))
+
+
+def average(numbers):
+    """Return the average of a list of numbers."""
+    return sum(numbers) / len(numbers)
