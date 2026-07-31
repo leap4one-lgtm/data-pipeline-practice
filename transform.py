47d57bbb9a6126a14e3c289e4c3bcdea04894e9d
leap4one-lgtm <leap4one@gmail.com>
2026-07-31 07:13:48 +0530

    Update transform.py
    chi chi

--- transform.py
+++ transform.py
@@ -2,7 +2,7 @@ def clean_numbers(numbers):
     """Remove duplicates and sort a list of numbers."""
     return sorted(set(numbers))
 
-
+#d be tgv
 def average(numbers):
     """Return the average of a list of numbers."""
     return sum(numbers) / len(numbers)
